a1085e431f11e3315f596c62782b9dfe200ace84
samuba <samuel.z.bach@gmail.com>
2025-04-25 16:08:44 +0200

    wip

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,23 +1,29 @@
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
 import type { AsyncRemoteCallback } from 'drizzle-orm/sqlite-proxy';
 import * as schema from './schema';
-import { dev } from '$app/environment';
+import { dev, building } from '$app/environment';
 import { getRequestEvent } from '$app/server';
 
 let d1Driver: AsyncRemoteCallback;
 
-let d1HttpDriverInstance: typeof import('../../drizzle-d1-http').d1HttpDriver;
+let d1HttpDriverInstance: typeof import('../../drizzle-d1-http').d1HttpDriver | undefined;
 
-if (dev) {
+if (dev || building) {
     if (!d1HttpDriverInstance) {
         d1HttpDriverInstance = (await import('../../drizzle-d1-http')).d1HttpDriver
     }
+
+    const driver = d1HttpDriverInstance;
+    if (!driver) {
+        throw new Error('Failed to load d1HttpDriver');
+    }
+
     d1Driver = async (sql: string, params: unknown[], method: "all" | "run" | "get" | "values") => {
         if (method === "values") {
-            const result = await d1HttpDriverInstance(sql, params, "all");
+            const result = await driver(sql, params, "all");
             return result;
         }
-        return d1HttpDriverInstance(sql, params, method);
+        return driver(sql, params, method);
     };
 } else {
     const requestEvent = getRequestEvent();
