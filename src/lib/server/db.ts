67859a9f4eef029f9e096aa61d7c131d29337733
samuba <samuel.z.bach@gmail.com>
2025-04-25 18:03:57 +0200

    wip

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/sqlite-proxy';
 import type { AsyncRemoteCallback } from 'drizzle-orm/sqlite-proxy';
 import * as schema from './schema';
 import { dev, building } from '$app/environment';
-// import { getRequestEvent } from '$app/server';
+import { getRequestEvent } from '$app/server';
 // import { d1HttpDriver } from '../../drizzle-d1-http';
 
 let d1Driver: AsyncRemoteCallback;
