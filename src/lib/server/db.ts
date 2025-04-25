c958150f4f336edb11eea91d8643cad2abc16b47
samuba <samuel.z.bach@gmail.com>
2025-04-25 17:58:00 +0200

    wip

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,19 +3,22 @@ import type { AsyncRemoteCallback } from 'drizzle-orm/sqlite-proxy';
 import * as schema from './schema';
 import { dev, building } from '$app/environment';
 import { getRequestEvent } from '$app/server';
-import { d1HttpDriver } from '../../drizzle-d1-http';
+// import { d1HttpDriver } from '../../drizzle-d1-http';
 
 let d1Driver: AsyncRemoteCallback;
 
 if (dev || building) {
-    import("dotenv/config");
-    d1Driver = async (sql: string, params: unknown[], method: "all" | "run" | "get" | "values") => {
-        if (method === "values") {
-            const result = await d1HttpDriver(sql, params, "all");
-            return result;
-        }
-        return d1HttpDriver(sql, params, method);
-    };
+    // import("dotenv/config");
+    // d1Driver = async (sql: string, params: unknown[], method: "all" | "run" | "get" | "values") => {
+    //     if (method === "values") {
+    //         const result = await d1HttpDriver(sql, params, "all");
+    //         return result;
+    //     }
+    //     return d1HttpDriver(sql, params, method);
+    // };
+    d1Driver = async () => {
+        return { rows: [] };
+    }
 } else {
     const requestEvent = getRequestEvent();
     d1Driver = requestEvent.platform?.env.DB
