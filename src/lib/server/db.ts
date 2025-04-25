59a47ab9ecca4671d226c925f79c01d56de40415
samuba <samuel.z.bach@gmail.com>
2025-04-25 18:09:17 +0200

    wip

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -6,24 +6,22 @@ import { getRequestEvent } from '$app/server';
 // import { d1HttpDriver } from '../../drizzle-d1-http';
 
 let d1Driver: AsyncRemoteCallback;
-dev && building;
 
-// if (dev || building) {
-// import("dotenv/config");
-// d1Driver = async (sql: string, params: unknown[], method: "all" | "run" | "get" | "values") => {
-//     if (method === "values") {
-//         const result = await d1HttpDriver(sql, params, "all");
-//         return result;
-//     }
-//     return d1HttpDriver(sql, params, method);
-// };
-d1Driver = async () => {
-    return { rows: [] };
+if (dev || building) {
+    // import("dotenv/config");
+    // d1Driver = async (sql: string, params: unknown[], method: "all" | "run" | "get" | "values") => {
+    //     if (method === "values") {
+    //         const result = await d1HttpDriver(sql, params, "all");
+    //         return result;
+    //     }
+    //     return d1HttpDriver(sql, params, method);
+    d1Driver = async () => {
+        return { rows: [] };
+    }
+} else {
+    const requestEvent = getRequestEvent();
+    d1Driver = requestEvent.platform?.env.DB
 }
-// } else {
-//     const requestEvent = getRequestEvent();
-//     d1Driver = requestEvent.platform?.env.DB
-// }
 
 export const db = drizzle(d1Driver, { schema });
 
