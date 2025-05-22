524bb687fa53227c0aeb68bb446b3105a34f53cc
samuba <samuel.z.bach@gmail.com>
2025-05-22 17:52:41 +0200

    wip

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -37,13 +37,13 @@
 				Zurück
 			</button>
 		</div>
-		<div class="card card-border bg-base-100 shadow-xl">
+		<div class="card sm:card-border bg-base-100 rounded-none shadow-xl sm:rounded-2xl">
 			{#if event.imageUrls && event.imageUrls.length > 0}
 				<div
-					class="rounded-t-2xl bg-cover bg-center"
+					class="bg-cover bg-center sm:rounded-t-2xl"
 					style="background-image: url({event.imageUrls[0]})"
 				>
-					<figure class="rounded-t-2xl shadow-sm backdrop-blur-md backdrop-brightness-85">
+					<figure class="shadow-sm backdrop-blur-md backdrop-brightness-85 sm:rounded-t-2xl">
 						<img
 							src={event.imageUrls[0]}
 							alt={event.name}
@@ -52,140 +52,133 @@
 					</figure>
 				</div>
 			{/if}
-			<div class="card-body text-base-content/90 items-center text-center">
-				<h1 class="card-title mb-4 text-xl font-bold">{event.name}</h1>
+			<div class="card-body text-base-content/90 w-full space-y-4">
+				<h1 class="card-title block w-full text-center text-xl font-bold">{event.name}</h1>
 
-				<div class="w-full space-y-4 text-left">
-					<div class="flex flex-wrap justify-center gap-4">
-						<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
-							<CalendarDots class="mr-2 size-6" />
-							<p class="text-md font-medium">
-								{formatTimeStr(event.startAt, event.endAt)}
-							</p>
-						</div>
-
-						{#if event.price}
-							<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
-								<Money class="mr-2 size-6" />
-								<p class="font-medium">{event.price}</p>
-							</div>
-						{/if}
-
-						{#if event.address?.length}
-							<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
-								<MapPin class="mr-1 size-6" />
-								{#if event.address}
-									<p class="font-medium">{formatAddress(event.address)}</p>
-								{/if}
-							</div>
-						{/if}
-
-						{#if event.sourceUrl}
-							<a
-								href={event.sourceUrl}
-								target="_blank"
-								rel="noopener noreferrer"
-								class="btn-primary btn"
-							>
-								Anmelden
-							</a>
-						{:else if event.contact}
-							<PopOver contentClass="bg-base-100 p-4">
-								{#snippet trigger()}
-									<button class="btn btn-primary">Anmelden</button>
-								{/snippet}
-								{#snippet content()}
-									<span class="word-wrap">
-										Der Veranstalter möchte Anmeldungen per {contactMethod} erhalten.
-									</span>
-									<div class="mt-3 flex justify-center">
-										{#if contactMethod === 'Telegram'}
-											<a
-												href={event.contact}
-												target="_blank"
-												rel="noopener noreferrer"
-												class="btn btn-primary"
-											>
-												<TelegramLogo class="size-5" />
-												Nachricht senden
-											</a>
-										{:else if contactMethod === 'WhatsApp'}
-											<a
-												href={event.contact}
-												target="_blank"
-												rel="noopener noreferrer"
-												class="btn btn-primary"
-											>
-												<WhatsAppLogo class="size-5" />
-												Nachricht senden
-											</a>
-										{:else if contactMethod === 'Telefon'}
-											<a
-												href={event.contact}
-												target="_blank"
-												rel="noopener noreferrer"
-												class="btn btn-primary"
-											>
-												<Phone class="size-5" />
-												Anrufen
-											</a>
-										{:else if contactMethod === 'Email'}
-											<a
-												href={event.contact}
-												target="_blank"
-												rel="noopener noreferrer"
-												class="btn btn-primary"
-											>
-												<Envelope class="size-5" />
-												Email senden
-											</a>
-										{/if}
-									</div>
-								{/snippet}
-							</PopOver>
-						{/if}
+				<div class=" flex flex-wrap justify-center gap-4">
+					<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
+						<CalendarDots class="mr-2 size-6" />
+						<p class="text-md font-medium">
+							{formatTimeStr(event.startAt, event.endAt)}
+						</p>
 					</div>
 
-					{#if event.description}
-						<p class="event-description whitespace-pre-wrap">
-							{@html event.description}
-						</p>
+					{#if event.price}
+						<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
+							<Money class="mr-2 size-6" />
+							<p class="font-medium">{event.price}</p>
+						</div>
 					{/if}
 
-					{#if event.host}
-						<div class="flex flex-wrap items-center gap-1.5 whitespace-pre-wrap">
-							<div class="flex items-center gap-1.5">
-								<Person class="size-6" />
-								<h2 class="font-medium">Organisation:</h2>
-							</div>
-							{#if event.hostLink}
-								<a
-									href={event.hostLink}
-									class="underline"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									{event.host}
-								</a>
-							{:else}
-								{event.host}
+					{#if event.address?.length}
+						<div class="bg-base-200 flex items-center justify-center rounded-full px-4 py-1.5">
+							<MapPin class="mr-1 size-6" />
+							{#if event.address}
+								<p class="font-medium">{formatAddress(event.address)}</p>
 							{/if}
 						</div>
 					{/if}
 
-					{#if event.tags && event.tags.length > 0}
-						<div>
-							<h2 class="mb-2 font-medium">Tags</h2>
-							<div class="flex flex-wrap gap-2">
-								{#each event.tags as tag}
-									<span class="badge badge-ghost">{tag}</span>
-								{/each}
-							</div>
-						</div>
+					{#if event.sourceUrl}
+						<a
+							href={event.sourceUrl}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="btn-primary btn"
+						>
+							Anmelden
+						</a>
+					{:else if event.contact}
+						<PopOver contentClass="bg-base-100 p-4">
+							{#snippet trigger()}
+								<button class="btn btn-primary">Anmelden</button>
+							{/snippet}
+							{#snippet content()}
+								<span class="word-wrap">
+									Der Veranstalter möchte Anmeldungen per {contactMethod} erhalten.
+								</span>
+								<div class="mt-3 flex justify-center">
+									{#if contactMethod === 'Telegram'}
+										<a
+											href={event.contact}
+											target="_blank"
+											rel="noopener noreferrer"
+											class="btn btn-primary"
+										>
+											<TelegramLogo class="size-5" />
+											Nachricht senden
+										</a>
+									{:else if contactMethod === 'WhatsApp'}
+										<a
+											href={event.contact}
+											target="_blank"
+											rel="noopener noreferrer"
+											class="btn btn-primary"
+										>
+											<WhatsAppLogo class="size-5" />
+											Nachricht senden
+										</a>
+									{:else if contactMethod === 'Telefon'}
+										<a
+											href={event.contact}
+											target="_blank"
+											rel="noopener noreferrer"
+											class="btn btn-primary"
+										>
+											<Phone class="size-5" />
+											Anrufen
+										</a>
+									{:else if contactMethod === 'Email'}
+										<a
+											href={event.contact}
+											target="_blank"
+											rel="noopener noreferrer"
+											class="btn btn-primary"
+										>
+											<Envelope class="size-5" />
+											Email senden
+										</a>
+									{/if}
+								</div>
+							{/snippet}
+						</PopOver>
 					{/if}
 				</div>
 
-				<div class="card-actions mt-6">
+				{#if event.description}
+					<p class="event-description prose">
+						{@html event.description}
+					</p>
+				{/if}
+
+				{#if event.host}
+					<div class=" flex flex-wrap items-center gap-1.5 text-[16px]">
+						<div class="flex items-center gap-1.5">
+							<Person class="size-6" />
+							<h2 class="font-medium">Organisation:</h2>
+						</div>
+						{#if event.hostLink}
+							<a href={event.hostLink} class="underline" target="_blank" rel="noopener noreferrer">
+								{event.host}
+							</a>
+						{:else}
+							{event.host}
+						{/if}
+					</div>
+				{/if}
+
+				{#if event.tags && event.tags.length > 0}
+					<div>
+						<h2 class="mb-2 font-medium">Tags</h2>
+						<div class="flex flex-wrap gap-2">
+							{#each event.tags as tag}
+								<span class="badge badge-ghost">{tag}</span>
+							{/each}
+						</div>
+					</div>
+				{/if}
+
+				<div class="flex w-full justify-center">
 					<a href="/" class="btn btn-sm">
 						<ArrowLeft class="mr-1 size-5" />
 						Zurück zur Übersicht
