be9b48b2083ea80a41d379710e9954d62e3570f4
samuba <samuel.z.bach@gmail.com>
2025-05-27 15:46:32 +0200

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: process.env.VERCEL ? undefined : 'nodejs23.x'
+		})
 	}
 };
 
