c8c14cea81d133d78b0e1be1c2a76a5a1025f139
samuba <samuel.z.bach@gmail.com>
2025-05-27 15:53:21 +0200

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,25 +7,25 @@
 	let { children } = $props();
 
 	let webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '');
-	onMount(async () => {
-		if (pwaInfo) {
-			const { registerSW } = await import('virtual:pwa-register');
-			registerSW({
-				immediate: true,
-				onRegistered(r) {
-					// uncomment following code if you want check for updates
-					// r && setInterval(() => {
-					//    console.log('Checking for sw update')
-					//    r.update()
-					// }, 20000 /* 20s for testing purposes */)
-					console.log(`SW Registered: ${r}`);
-				},
-				onRegisterError(error) {
-					console.log('SW registration error', error);
-				}
-			});
-		}
-	});
+	// onMount(async () => {
+	// 	if (pwaInfo) {
+	// 		const { registerSW } = await import('virtual:pwa-register');
+	// 		registerSW({
+	// 			immediate: true,
+	// 			onRegistered(r) {
+	// 				// uncomment following code if you want check for updates
+	// 				// r && setInterval(() => {
+	// 				//    console.log('Checking for sw update')
+	// 				//    r.update()
+	// 				// }, 20000 /* 20s for testing purposes */)
+	// 				console.log(`SW Registered: ${r}`);
+	// 			},
+	// 			onRegisterError(error) {
+	// 				console.log('SW registration error', error);
+	// 			}
+	// 		});
+	// 	}
+	// });
 </script>
 
 <svelte:head>
