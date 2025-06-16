c3665ded247ea4ace73121effeea75d069e69e6c
samuba <samuel.z.bach@gmail.com>
2025-06-16 13:43:31 +0200

    wip

--- src/routes/telegram/message/+server.ts
+++ src/routes/telegram/message/+server.ts
@@ -4,6 +4,7 @@ import { bot } from './bot';
 export const POST: RequestHandler = async ({ request }) => {
     try {
         const data = await request.json();
+        console.log("http payload", data);
         bot.handleUpdate(data)
     } catch (error) {
         console.error(error);
