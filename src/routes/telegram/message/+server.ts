01dc750fb946b9e93c11828812f1a37f4593aa3c
samuba <samuel.z.bach@gmail.com>
2025-06-16 13:48:42 +0200

    wip

--- src/routes/telegram/message/+server.ts
+++ src/routes/telegram/message/+server.ts
@@ -1,11 +1,13 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { bot } from './bot';
+import { waitUntil } from '@vercel/functions';
 
 export const POST: RequestHandler = async ({ request }) => {
     try {
         const data = await request.json();
         console.log("http payload", data);
-        bot.handleUpdate(data)
+
+        waitUntil(bot.handleUpdate(data))
     } catch (error) {
         console.error(error);
     }
