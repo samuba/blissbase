b873721c3ff677acfa48f934176afa81ee14aa03
samuba <samuel.z.bach@gmail.com>
2025-06-24 19:03:14 +0200

    posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,19 @@
 import type { MetaTagsProps } from 'svelte-meta-tags';
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
 
 export const load = ({ url }) => {
+    if (browser) {
+        posthog.init(
+            'phc_B5MC1SXojC0n2fXhIf9WCDk6O2cqhdLk7SQCT7eldqZ',
+            {
+                api_host: 'https://eu.i.posthog.com',
+                defaults: '2025-05-24',
+                person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+            }
+        )
+    }
+
 
     // --- Meta Tags ---
     const baseUrl = new URL(url.pathname, url.origin).href
