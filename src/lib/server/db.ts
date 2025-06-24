e33bca612d765874ab968060b4817698f12cadf6
samuba <samuel.z.bach@gmail.com>
2025-06-24 18:20:47 +0200

    neon -> supabase

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -3,12 +3,12 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema';
 
-if (!process.env.DATABASE_URL_NEON) {
-    throw new Error('DATABASE_URL_NEON is not set');
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set');
 }
 
 export const db = drizzle({
-    client: postgres(process.env.DATABASE_URL_NEON, {
+    client: postgres(process.env.DATABASE_URL, {
         prepare: false // Disable prefetch as it is not supported for "Transaction" pool mode
     }),
     schema,
