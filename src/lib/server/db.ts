56a61ea3f3301a48102f8d7e508861e070de5c7b
samuba <samuel.z.bach@gmail.com>
2025-06-24 17:10:03 +0200

    neon -> supabase

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,20 +1,16 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import * as schema from './schema';
-import ws from 'ws';
-
-neonConfig.webSocketConstructor = ws;
-
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
 
 if (!process.env.DATABASE_URL) {
     throw new Error('DATABASE_URL is not set');
 }
 
 export const db = drizzle({
-    client: neon(process.env.DATABASE_URL),
+    client: postgres(process.env.DATABASE_URL, {
+        prepare: false // Disable prefetch as it is not supported for "Transaction" pool mode
+    }),
     schema,
     casing: 'snake_case'
 });
