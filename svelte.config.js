c4bacc039fb02798fcb39abfe3b451e628620db0
samuba <samuel.z.bach@gmail.com>
2025-08-07 14:39:25 +0200

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -10,8 +10,7 @@ const config = {
 		},
 		adapter: adapter({
 			// vercel does not support nodejs23.x
-			runtime: process.env.VERCEL ? 'nodejs22.x' : 'nodejs23.x',
-			split: true
+			runtime: process.env.VERCEL ? 'nodejs22.x' : 'nodejs23.x'
 		}),
 		version: {
 			pollInterval: 60_000 * 1
