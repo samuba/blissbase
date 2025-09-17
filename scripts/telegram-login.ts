d7be9b797dc8fe6a3adebadfae0f084216ce02f3
samuba <samuel.z.bach@gmail.com>
2025-09-17 16:19:26 +0200

    wip

---
+++ scripts/telegram-login.ts
@@ -0,0 +1,32 @@
+import { TelegramClient } from "telegram";
+import { StringSession } from "telegram/sessions";
+import readline from "readline";
+import 'dotenv/config'
+
+const apiId = Number(process.env.TELEGRAM_APP_ID!);
+const apiHash = process.env.TELEGRAM_APP_HASH!;
+const stringSession = new StringSession(""); // fill this later with the value from session.save()
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
+await client.start({
+    phoneNumber: async () =>
+        new Promise((resolve) =>
+            rl.question("Please enter your number: ", resolve)
+        ),
+    password: async () =>
+        new Promise((resolve) =>
+            rl.question("Please enter your password: ", resolve)
+        ),
+    phoneCode: async () =>
+        new Promise((resolve) =>
+            rl.question("Please enter the code you received: ", resolve)
+        ),
+    onError: (err) => console.log(err),
+});
+console.log("Login succesfull")
+console.log("Session key:", client.session.save()); // Save this string to avoid logging in again
