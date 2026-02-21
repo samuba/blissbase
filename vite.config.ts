e8cfb2e5ba83653b9b03209cbf34c59d4b76e995
samuba <samuel.z.bach@gmail.com>
2026-02-21 21:18:18 +0700

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -36,16 +36,6 @@ export default defineConfig({
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable'
-					},
-					{
-						src: 'pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: 'pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
 					}
 				]
 			}
