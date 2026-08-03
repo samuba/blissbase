d129869cfc8fb4f102eb40cf2ba25aa861c8b852
samuba <samuel.z.bach@gmail.com>
2026-08-03 19:55:11 +0200

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		},
 		experimental: {
 			remoteFunctions: true,
-			forkPreloads: true // in prod this kills the tab
+			forkPreloads: false // diagnosing hang on offerings hover-preload
 		}
 	},
 	vitePlugin: {
