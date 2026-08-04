291ce26bd9a7d67b0bbdbcd85241b17a7574858c
samuba <samuel.z.bach@gmail.com>
2026-08-04 14:25:00 +0200

    wip

--- src/lib/assets/hero-images.ts
+++ src/lib/assets/hero-images.ts
@@ -3,7 +3,7 @@ import type { Picture } from 'vite-imagetools';
 
 import mobile from './testcover6.jpeg?enhanced';
 import tablet from './testcover2.jpeg?enhanced';
-import desktop from './testcover3.jpeg?enhanced&format=webp;jpeg';
+import desktop from './testcover3.jpeg?enhanced&quality=100';
 
 export const heroMobile = mobile as Picture;
 export const heroTablet = tablet as Picture;
