42161f7f50842c653ef6d66d1ddb55550be825e1
samuba <samuel.z.bach@gmail.com>
2026-08-15 16:40:28 +0200

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,7 +89,7 @@
 	<div class="col-start-1 row-start-1 z-10 container mx-auto sm:w-2xl flex flex-col gap-4 sm:gap-8 items-center justify-center">
 		<div class="flex flex-col gap-4 w-full items-center justify-center px-4">
 			<div class="flex justify-center items-center gap-3">
-				<img src="/logo-transparent.svg" alt="" class="size-12 md:size-16 drop-shadow-xl" data-testid="hero-logo" />
+				<img src="/logo-transparent.svg" alt="" class="size-12 md:size-16 lg:size-18 drop-shadow-xl" data-testid="hero-logo" />
 				<h1 class="md:text-5xl text-4xl font-semibold text-base-100 font-brand" >
 					Blissbase
 				</h1>
